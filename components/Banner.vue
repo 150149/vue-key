d718f6d4218a9128e3b816cb68f2c5f9944c8a3f
150149 <1802796278@qq.com>
2021-03-03 21:52:11 +0800

    2020.11.17
    估计要弃坑了

---
+++ components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="banner-one" id="banner">
+    <span class="banner-one__shape-1"></span>
+    <span class="banner-one__shape-2"></span>
+    <span class="banner-one__shape-3"></span>
+    <span class="banner-one__shape-4"></span>
+    <div class="container">
+      <div class="banner-one__moc">
+        <img src="/assets/images/mocs/banner-moc-1-1.png" alt="Awesome Image" />
+      </div><!-- /.banner-one__moc -->
+      <div class="row">
+        <div class="col-xl-6 col-lg-8">
+          <div class="banner-one__content">
+
+            <h3 class="banner-one__title">耀世中洲</h3><!-- /.banner-one__title -->
+            <p class="banner-one__text">致力于做最优秀的魔戒服务器</p>
+            <!-- /.banner-one__text -->
+            <a href="https://share.weiyun.com/OhnSGx92" class="banner-one__btn thm-btn "><span>下载客户端</span></a><!-- /.thm-btn -->
+          </div><!-- /.banner-one__content -->
+        </div><!-- /.col-lg-6 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "Banner"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <footer class="site-footer">
+      <div class="site-footer__upper">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="footer-widget footer-widget__about">
+                <img src="/assets/images/resources/logo-dark.png" width="119" alt="" class="footer-widget__logo">
+
+                <!-- /.footer-widget__contact -->
+
+
+              </div><!-- /.footer-widget -->
+            </div><!-- /.col-lg-2 -->
+            <div class="col-lg-3 d-flex justify-content-between footer-widget__links-wrap">
+              <div class="footer-widget footer-widget__about">
+                <!-- /.footer-widget__contact -->
+                <p class="footer-widget__contact"><a>耀世中洲</a></p>
+
+              </div><!-- /.footer-widget -->
+
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-3">
+              <p class="footer-widget__contact">QQ群：1054842957</p><!-- /.footer-widget__contact -->
+            </div><!-- /.col-lg-4 -->
+          </div><!-- /.row -->
+        </div><!-- /.container -->
+      </div><!-- /.site-footer__upper -->
+      <div class="site-footer__bottom">
+        <div class="container">
+          <div class="inner-container text-center">
+            <p class="site-footer__copy">&copy; copyright 2020 by <a >150149</a></p>
+            <!-- /.site-footer__copy -->
+          </div><!-- /.inner-container -->
+        </div><!-- /.container -->
+      </div><!-- /.site-footer__bottom -->
+    </footer>
+
+
+
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Footer",
+      mounted() {
+
+        $(window).on('scroll', function () {
+          if ($('.scroll-to-top').length) {
+            var strickyScrollPos = 100;
+            if ($(window).scrollTop() > strickyScrollPos) {
+              $('.scroll-to-top').fadeIn(500);
+            } else if ($(this).scrollTop() <= strickyScrollPos) {
+              $('.scroll-to-top').fadeOut(500);
+            }
+          }
+        })
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
